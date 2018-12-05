74c9fc0731dc71f4178c261ff43f20063bb5a871
colombelli <xcolombelli@gmail.com>
2018-12-05 02:56:41 -0200

    GUI implementada

---
+++ Etapa 2/GUI.py
@@ -0,0 +1,241 @@
+from tkinter import *
+from tkinter import ttk   # para a combobox
+from tkinter import filedialog # para procurar arquivo
+from TrabFormais2 import *
+
+
+
+
+def limpaOutputbox():   #simplesmente limpa a outputbox
+
+    output.config(state=NORMAL)   # habilita edição da text box
+    output.delete(0.0, END)
+    return
+
+
+
+# função responsável pela navegação no explorer para achar o txt e carregar o autômato
+def carregarAutomato():
+
+    # define-se que vai alterar as variáveis globais abaixo
+    global listaDePalavras
+    global listaSimbConcatenados
+    global automato
+
+    try:   # tenta carregar, ler e converter o autômato
+        window.fileName = filedialog.askopenfilename(title="select file", filetypes=(("text files", "*.txt"), ("all files", "*.*")))   # pega nome do arquivo
+        automato = converte(window.fileName)
+        limpaOutputbox()
+        output.config(state=DISABLED) # desabilita edição da text box novamente
+
+
+    except ValueError:   # caso dê algum problema, avisa na outputbox
+        limpaOutputbox()
+        output.insert(END, "Erro ao converter o autômato! Certifique-se de adicionar um arquivo válido.\n\n")   # insere resultado
+        output.config(state=DISABLED) # desabilita edição da text box novamente
+
+
+    listaDePalavras.clear()
+    listaSimbConcatenados.clear()
+
+    return
+
+
+
+# função que checa se o automato aceita ou rejeita a palavra passada para o automato definido globalmente
+def reconhecerPalavras():
+
+    # define-se que vai alterar as variáveis globais abaixo
+    global listaDePalavras
+    global listaSimbConcatenados
+
+    limpaOutputbox()
+
+    # se lista vazia retorna avisando que deve-se inserir palavras
+    if not listaDePalavras:
+
+        output.insert(END, "Erro! Você deve inserir palavras a serem reconhecidas pelo autômato.\n\n")   # insere resultado
+        output.config(state=DISABLED) # desabilita edição da text box novamente
+
+        listaDePalavras.clear()
+        listaSimbConcatenados.clear()
+
+        return
+
+
+
+    if not automato:   # caso um automato não tenha  sido carregado e convertido devidamente, avisa na outputbox e retorna
+
+            limpaOutputbox()
+            output.insert(END, "Erro! Certifique-se de adicionar um arquivo válido para seu autômato.\n\n")   # insere resultado
+            output.config(state=DISABLED) # desabilita edição da text box novamente
+
+            listaDePalavras.clear()
+            listaSimbConcatenados.clear()
+
+            return
+
+
+    counter = 0   # utilizado apenas para indicar na string de saída qual palavra foi/não foi reconhecida (pegando da lista das concatenadas)
+    # feitas as validações, checa aceitação do autômato para as palavras inseridas
+    for palavra in listaDePalavras:
+        palavra = palavra.split(",") # salva em palavra uma lista com cada simbolo da palavra de entrada
+        resultado = checaSeAceita(automato, palavra)
+
+        output.config(state=NORMAL)   # habilita edição da text box
+        # a string abaixo será num formato:
+        # 1. aabba   ACEITA
+        output.insert(END, " " + str(counter+1) + ". " + listaSimbConcatenados[counter] + "   " + resultado + "\n\n")   # insere resultado
+        output.config(state=DISABLED) # desabilita edição da text box novamente
+
+        counter += 1
+
+
+    listaDePalavras.clear()
+    listaSimbConcatenados.clear()
+
+    return
+
+
+
+# função que adiciona mais palavras a serem reconhecidas na lista de palavras
+def adicionarPalavra():
+
+    # define-se que vai alterar as variáveis globais abaixo
+    global listaDePalavras
+    global listaSimbConcatenados
+
+    palavra = input.get()   # pega o input da entrybox
+    input.delete(0, 'end')   # deleta o que foi escrito na entrybox
+
+
+    if palavra == "":   # se o usuário não digitar nada, retorna avisando na box de output
+
+        output.config(state=NORMAL)   # habilita edição da text box
+        output.insert(END, "Entrada inválida! Digite a palavra separando os símbolos por vírgulas.\n\n")   # insere resultado
+        output.config(state=DISABLED) # desabilita edição da text box novamente
+
+        return
+
+    # caso seja uma entrada válida, junta na lista de palavras a serem reconhecidas
+    listaDePalavras.append(palavra)
+
+    # e concatena a string juntando também na lista de palavras com símbolos concatenados
+    palavra = palavra.replace(',', '')
+    listaSimbConcatenados.append(palavra)
+
+    # avisa na box que a palavra foi adicionada
+    output.config(state=NORMAL)   # habilita edição da text box
+    output.insert(END, "Palavra adicionada: " + palavra + "\n\n")   # insere texto
+    output.config(state=DISABLED) # desabilita edição da text box novamente
+
+    return
+
+
+
+# função que apaga a outputbox e mostra os créditos
+def showCredits():
+
+    limpaOutputbox()
+    output.insert(END, "By\n\nFelipe Colombelli: github.com/colombelli/\nRafael Ferreira: github.com/Rafaelfferreira/\nDaives Kawon Chu: github.com/daivesk/\n\nProjeto: https://github.com/Rafaelfferreira/TrabFormais")
+    output.config(state=DISABLED) # desabilita edição da text box novamente
+
+
+    return
+
+
+
+
+
+
+# variável utilizada para carregar o automato e convertê-lo, começa por padrão com o autômato definido como cenário
+automato = converte("automatop.txt")
+
+listaDePalavras = []   # cada item é uma palavra onde as strings estão separadas por vírgula
+listaSimbConcatenados = []   # cada item é uma palavra (sem as vírgulas), i.e, com símbolos concatenados
+
+
+
+
+
+
+'''
+
+    DEFINIÇÕES DA GUI
+
+'''
+
+
+
+
+# main
+window = Tk()
+
+# definição do tamanho da janela
+width_window = 403
+height_window = 425
+
+# pega tamanho da tela do usuário
+screenW = window.winfo_screenwidth()
+screenH = window.winfo_screenheight()
+
+# coordenadas para centralizar janela
+xpos = (screenW/2) - (width_window/2)
+ypos = (screenH/2) - (height_window/2)
+
+# seta um título na janela
+window.title("Automatop")
+# seta tamanho da janela e posição na tela
+window.geometry("%dx%d+%d+%d" % (width_window, height_window, xpos, ypos))
+# seta cor de fundo da janela
+window.configure(background="white")
+# bloqueia redimensionamento da janela
+window.resizable(width=False, height=False)
+# define icone da janela
+window.iconbitmap('images/icon.ico')
+
+
+
+# Menu para adicionar autômato ou fechar programa
+menu = Menu(window) # menu topo da janela
+window.config(menu=menu)
+
+subMenu = Menu(menu, tearoff=0) # submenu dropdown
+menu.add_cascade(label="File", menu=subMenu)
+menu.add_command(label="Credits", command=showCredits)
+menu.add_command(label="Exit", command=window.destroy)
+subMenu.add_command(label="Carregar Autômato", command=carregarAutomato)
+
+
+
+
+
+# logo automatop
+logo = PhotoImage(file="images/automatop_400.gif")
+# o columnspan do grid define que a imagem vai ocupar duas colunas
+Label (window, image=logo, bg="white") .grid(row=0, column=0, sticky=EW)
+# o columnspan do grid define que a imagem vai ocupar duas colunas
+Label (window, text="Digite a palavra separando os símbolos por vírgula:", bg="white", fg="black", font="Arial 10") .grid(row=1, column=0, pady=(10,0), sticky=EW)
+
+# caixa de input
+input = Entry(window, font="none 11", bg="white")
+input.grid(row=2, column=0, padx=10, sticky=EW)
+
+# botão de adicionar mais palavras
+Button(window, text="Adicionar", command=adicionarPalavra) .grid(row=3, column=0, padx=(130,0), pady=10, sticky=W)
+
+# botão de reconhecer palavras
+Button(window, text="Reconhecer", command=reconhecerPalavras) .grid(row=3, column=0, padx=(200,0), pady=10, sticky=W)
+
+# cria textbox de output (onde os resultados vão ser printados)
+output = Text (window, width=45, height=12, wrap=WORD)
+output.grid(row=4, column=0, padx=6, pady=15, sticky=W)
+output.insert(END, "")
+output.config(state=DISABLED)   # não permite mexer na text box (tem que habilitar para inserir texto mesmo com .insert())
+# scrollbar  para textbox:
+scrollb = Scrollbar(window, command=output.yview)
+scrollb.grid(row=4, column=0, padx=(370,0), pady=15, ipady=72)   # sendo ipady o tamanho da scrollbar
+output['yscrollcommand'] = scrollb.set
+
+
+window.mainloop()
--- Etapa 2/TrabFormais2.py
+++ Etapa 2/TrabFormais2.py
@@ -3,9 +3,9 @@ import os
 import copy
 
 #Classe do automato de entrada (não deterministico)
-class automato:        
+class automato:
     def __init__(self, arqtxt): #recebe como parametro um arquivo texto contendo o automato
-        
+
         #Retorna os simbolos do alfabeto, os estados do automato, o estado inicial e os estados finais
         quadrupla = re.search("\(.*?\)",arqtxt) #Procura a parte da string entre parenteses e a retorna (com os parenteses) dentro de um objeto match
         quadrupla = quadrupla.group(0) #Extrai a string do objeto match
@@ -32,12 +32,12 @@ class automato:
         self.inicial = restante[0]
         if(len(restante) == 2):
             self.finais = restante[1]
-        else:        
+        else:
             #filtra os estados finais
             finais = dupla[len(self.inicial)+1:]
             finais = finais[1:len(finais)-1]
             self.finais = finais.split(',')
-            
+
         #Salva a tabela de transição como uma lista de listas
         #Da um slice em dados a partir do index inicial da palavra Prog + o tamanho da palavra prog + 1 (pois tem um '\n' dps de prog)
         transitiv_raw = (arqtxt[arqtxt.index('Prog') + len('Prog') + 1:])
@@ -47,16 +47,16 @@ class automato:
         for x in transitiv_raw: #itera por cada elemento da tabela de transitividade
             inst = [] #inicializa a lista que vai salvar os 3 elemento de cada transição
             x = x.split('=') #divide o estado inicial + entrada do estado resultante
-    
+
             atual = x[0][1:len(x[0])-1] #salva na variavel atual o estado atual + a variavel de entrada sem os parenteses
             atual = atual.split(',') #faz com que atual contenha uma lista contendo o estado atual e a variavel
             inst.append(atual[0]) #salva o estado atual
             inst.append(atual[1]) #salva a variavel da transição
             inst.append(x[1]) #salva o estado resultante no terceiro espaço da lista
-        
+
             transitiv.append(inst) #salva a lista interna na lista externa
         self.transitiv = transitiv
-        
+
 #classe que vai conter o automato determinístico
 class automatoP:
     def __init__(self,automato):
@@ -64,36 +64,36 @@ class automatoP:
         self.simbolos = []
         self.inicial  = automato.inicial #Já é instanciado com o mesmo estado inicial do automato de entrada
         self.finais = []
-        self.transitiv = [] 
+        self.transitiv = []
 
 def recursao(estado, simbolos, transicoes, visited, automatoDFA):
-    
+
     # se o estado já foi checado e tratado, simplesmente retorna e continua a recursão
     if estado in visited:
         return
 
-        
+
     # caso não, antes de tratá-lo, a primeira coisa para garantir que a recursão não entre em loop é justamente sinalizar como já visitado:
     visited.append(estado)
-    
-    for simbolo in simbolos: 
-            
+
+    for simbolo in simbolos:
+
         # coleta todos os estados alcançáveis por aquele determinado estado recebendo aquele determinado símbolo
         listaEstadosAlcancados = []
-        
+
         # o estado sempre será tratado como uma lista de estados (que depois serão concatenados em um único):
         for e in estado:
             for t in range(len(transicoes)):
                 # checa nas transições se é o estado e a transição atuais e se o resultado já não está na lista de estados alcançados
                 if (e == transicoes[t][0]) and (simbolo == transicoes[t][1]) and not (transicoes[t][2] in listaEstadosAlcancados):
                     listaEstadosAlcancados.append(transicoes[t][2])
-               
-        
+
+
         #  checa se a lista de estados alcançados é vazia
         if not listaEstadosAlcancados:
             continue   # se for, skipa  uma iteração
-            
-        
+
+
         # sempre dá um sort pra garantir que não se criará um estado novo que na verdade é igual a um já existente
         listaEstadosAlcancados.sort()
         # depois dessa coleta, essa lista de estados vai virar um estado só do  automato determinístico final
@@ -102,22 +102,22 @@ def recursao(estado, simbolos, transicoes, visited, automatoDFA):
             automatoDFA.simbolos.append(simbolo)  # adiciona o simbolo atual ao automato determinístico final
         if not(estado in automatoDFA.estados):
             automatoDFA.estados.append(estado)   # adiciona  o estado atual ao automato determinístico final
-                   
+
         recursao(listaEstadosAlcancados, simbolos, transicoes, visited, automatoDFA)
 
 #função que faz a conversão do automato ND para Deterministico
 def converte(arquivo):
     openfile = open(arquivo,"r", encoding="utf-8",) #Lembrar de incluir o encoding para que ele leia os acentos corretamente
     dados = openfile.read()
-    
+
     automatoND = automato(dados) #le o arquivo e cria um automato nao deterministico
     automatoDFA = automatoP(automatoND) #instancia o automato deterministico
-    
+
     visited = [] #estados  já visitados
-    
+
     recursao([automatoDFA.inicial], automatoND.simbolos, automatoND.transitiv, visited, automatoDFA) #preenche o automato deterministico
-    
-    # agora temos que transformar as listas de estados em um estado só representado pela concatenação dos itens da lista:    
+
+    # agora temos que transformar as listas de estados em um estado só representado pela concatenação dos itens da lista:
     # primeiro na lista de estados:
     counter = 0
     for estado in automatoDFA.estados:
@@ -128,7 +128,7 @@ def converte(arquivo):
             automatoDFA.estados[counter] = string
         counter +=1
 
-    
+
     # depois fazemos o mesmo para as transições:
     counter = 0
     for trans in automatoDFA.transitiv:
@@ -138,7 +138,7 @@ def converte(arquivo):
             for q in trans[0]:   # para cada estado dentro desse estado que deveria ser único, concatena-os
                 string = string+q
             automatoDFA.transitiv[counter][0] = string
-        
+
         # trata estado final da transição
         if type(trans[2]) == list:   # se for um estado ou mais em forma de lista, converte
             string = ""
@@ -153,7 +153,7 @@ def converte(arquivo):
         finais = automatoND.finais
         if type(finais) == str: #transforma em uma lista de 1 elemento se o estado final for unico
             finais = [finais]
-    
+
         for efinal in finais:
             if (efinal in estado) and (not(estado in automatoDFA.finais)): #procura a substring efinal nos estado e confere se ele ja nao esta salvo com um estado final do automato deterministico
                 automatoDFA.finais.append(estado)
@@ -161,10 +161,10 @@ def converte(arquivo):
     return automatoDFA
 
 def checaSeAceita(automato,palavra):
-    
+
     # começa no estado inicial sempre
     eAtual = automato.inicial
-    
+
     # vai lendo os simbolos da palavra e tentando percorrer o autômato
     for simbolo in palavra:
         transValida = False   # se encontrar uma transição válida, muda
@@ -175,21 +175,21 @@ def checaSeAceita(automato,palavra):
                 eAtual = trans[2]
                 transValida = True
                 break
-        
+
         if transValida:
             continue   # vai para o próximo símbolo
-        
+
         # se não encontrar, retorna com "REJEITA"
         return "REJEITA"
-    
-    
+
+
     # se finalizou-se o loop sem retornar "REJEITA", testa se o estado atual é um estado final
     if eAtual in automato.finais:
         return "ACEITA"
-    
+
     else:
-        return "REJEITA"   
-    
+        return "REJEITA"
+
 #função a ser chamada para passar as palavras que serão testadas
 def reconhecimento(automato):
     listaDePalavras = [] #lista de palavras a serem testadas
@@ -201,7 +201,7 @@ def reconhecimento(automato):
     while True: #permite a entrada de mais palavras
         print('Deseja fornecer mais palavras? s/n')
         select = input()
-    
+
         if select == "s":
             print('Digite uma palavra separando os símbolos por vírgula, sem espaço, para verificar seu reconhecimento:')
             listaDePalavras.append(input())
@@ -227,9 +227,9 @@ def main():
 
 if __name__ == "__main__":
 	main()
-	
-	
-	
+
+
+
 	'''
 	Exemplos de palavras que sao aceitas no arquivo automatop.txt:
 
